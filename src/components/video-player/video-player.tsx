ddd4e19f6365afcb4e3862658a2eaa659ae65311
Christian Cook <christian@elixel.co.uk>
2017-09-30 12:56:12 +0100

    manage user hover state better

--- src/components/video-player/video-player.tsx
+++ src/components/video-player/video-player.tsx
@@ -5,27 +5,37 @@ import { Component, Prop, Listen, Element, State } from '@stencil/core';
     styleUrl: 'video-player.scss'
 })
 export class VideoPlayer {
+    // Browser conditions
+    private isSafari = navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1 ? true: false;
+    
+    // Props to configure the player options
     @Prop() url: string;
     @Prop() poster: string;
 
+    // Player state
     @State() isFullscreen: boolean = false;
     @State() isPlaying: boolean = false;
     @State() isMuted: boolean = false;
     @State() progress: number = 0.01;
     @State() duration: number = 1;
     @State() volume: number = 1;
+    @State() userFocus: boolean = true;
 
+    // DOM elements
     @Element() element: HTMLElement;
-
     private videoElement: any;
 
-    private isSafari = navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1 ? true: false;
-
+    // Internal states
     private wasPlaying: boolean; // maintains play state whilst scrubbing
+    private userFocusTimeout: NodeJS.Timer;
 
     componentDidLoad() {
         this.videoElement = this.element.querySelector('video-element');
     }
+
+    /**
+     * Manages playback state
+     */
 
     @Listen('play')
     playHandler() {
@@ -39,27 +49,18 @@ export class VideoPlayer {
         this.videoElement.pauseVideo();
     }
 
-    @Listen('mute')
-    muteHandler() {
-        this.isMuted = true;
-        this.videoElement.muteVideo();
-    }
-
-    @Listen('unmute')
-    unmuteHandler() {
-        this.isMuted = false;
-        this.videoElement.unmuteVideo();
-    }
-
-    @Listen('timeupdate')
-    timeupdateHandler(event) {
-        this.progress = event.detail;
-    }
+    /**
+     * Manages player metadata state
+     */
 
     @Listen('duration')
     durationHandler(event) {
         this.duration = event.detail;
     }
+
+    /**
+     * Manages seek state
+     */
 
     @Listen('seekStart')
     seekStartHandler(event) {
@@ -78,6 +79,15 @@ export class VideoPlayer {
         this.videoElement.seekTo(event.detail);
         if (this.wasPlaying) this.playHandler();
     }
+    
+    @Listen('timeupdate')
+    timeupdateHandler(event) {
+        this.progress = event.detail;
+    }
+
+    /**
+     * Manages volume state
+     */
 
     @Listen('volume')
     volumeHandler(event) {
@@ -86,6 +96,59 @@ export class VideoPlayer {
         else this.isMuted = false;
         this.videoElement.setVolume(event.detail);
     }
+    
+    @Listen('mute')
+    muteHandler() {
+        this.isMuted = true;
+        this.videoElement.muteVideo();
+    }
+
+    @Listen('unmute')
+    unmuteHandler() {
+        this.isMuted = false;
+        this.videoElement.unmuteVideo();
+    }
+
+    /**
+     * Manages full screen state
+     */
+
+    @Listen('enterFullscreen')
+    enterFullscreen() {
+        if (!this.isSafari) this.element.webkitRequestFullscreen();
+        else this.videoElement.enterFullscreen();
+    }
+
+    @Listen('exitFullscreen')
+    exitFullscreen() {
+        document.webkitExitFullscreen();
+    }
+
+    @Listen('webkitfullscreenchange')
+    fullscreenchangeHandler() {
+        this.isFullscreen = document.webkitIsFullScreen;
+    }
+
+    /**
+     * Manages user focus state
+     */
+    @Listen('mousemove')
+    mousemoveHandler() {
+        this.userFocus = true;
+        clearTimeout(this.userFocusTimeout)
+        this.userFocusTimeout = setTimeout(() => {
+            this.userFocus = false;
+        }, 3000);
+    }
+
+    @Listen('mouseleave')
+    mouseoutHandler() {
+        this.userFocus = false;
+    }
+    
+    /**
+     * Manages global keyboard controls for the player
+     */
 
     @Listen('body:keyup')
     keyboardHandler(keyboardEvent) {
@@ -110,22 +173,6 @@ export class VideoPlayer {
         }
     }
 
-    @Listen('enterFullscreen')
-    enterFullscreen() {
-        if (!this.isSafari) this.element.webkitRequestFullscreen();
-        else this.videoElement.enterFullscreen();
-    }
-
-    @Listen('exitFullscreen')
-    exitFullscreen() {
-        document.webkitExitFullscreen();
-    }
-
-    @Listen('webkitfullscreenchange')
-    fullscreenchangeHandler() {
-        this.isFullscreen = document.webkitIsFullScreen;
-    }
-
     render() {
         let audioControls = null;
         if (!this.isSafari) {
@@ -136,7 +183,7 @@ export class VideoPlayer {
         }
         return ([
             <video-element src={this.url} poster={this.poster}></video-element>,
-            <control-bar visible={!this.isPlaying}>
+            <control-bar visible={!this.isPlaying || this.userFocus}>
                 <scrub-bar progress={this.progress} duration={this.duration}></scrub-bar>
                 <play-button playing={this.isPlaying}></play-button>
                 <time-label time={this.progress}></time-label>
